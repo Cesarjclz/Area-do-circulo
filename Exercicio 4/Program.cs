fba1448fb23c3ceb17b19aa59620cbb1ae825f25
César Aguiar <Cesarphz21@gmail.com>
2021-10-20 12:19:04 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio 4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercicio_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            const double pi = 3.1415;
+            double area;
+            double raio;
+          
+            Console.WriteLine("Qual o valor do raio do circulo: ");
+            raio = double.Parse(Console.ReadLine());
+           
+            area = pi * (Math.Pow(raio, 2));
+            Console.WriteLine("A area do circulo é: {0}  ", area);
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
